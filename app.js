25b394d76078615c273f8116023af91b066a0719
Tomesyy <adetomiwaadesanya01@gmail.com>
2019-07-01 21:36:19 +0100

    heroku

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ var commentsRoutes       = require('./routes/comments'),
     campgroundRoutes     = require('./routes/campgrounds'),
     indexRoutes           = require('./routes/index');
 
-mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true});
+var url = process.env.DATABASEURL || 'mongodb://localhost:27017/yelp_camp'
+mongoose.connect(url, {useNewUrlParser: true});
+// mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
